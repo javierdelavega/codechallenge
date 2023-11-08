676ed36d140003fd2066ef8b2b46bdd86767da4e
Javier de la Vega <javier@smartidea.es>
2023-11-08 18:04:47 +0100

    jenkins coverage report

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -15,6 +15,9 @@ apt-get install -yqq git iputils-ping libzip-dev zip
 # Enable zip extension and db drivers
 docker-php-ext-install zip mysqli pdo_mysql
 
+# Xdebug
+pecl install xdebug && docker-php-ext-enable xdebug
+
 # Install composer
 php -r "readfile('https://getcomposer.org/installer');" > composer-setup.php
 php composer-setup.php --install-dir=/usr/local/bin --filename=composer
