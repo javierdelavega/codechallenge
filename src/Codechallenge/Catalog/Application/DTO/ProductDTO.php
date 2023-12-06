1f2d6006d71b715abe3cc043cab2fe51a57a583b
Javier de la Vega <javier@smartidea.es>
2023-12-07 00:42:09 +0100

    fix: bug in product fixtures and product catalog

--- src/Codechallenge/Catalog/Application/DTO/ProductDTO.php
+++ src/Codechallenge/Catalog/Application/DTO/ProductDTO.php
@@ -14,14 +14,14 @@ readonly class ProductDTO
     /**
      * Constructor.
      *
-     * @param ProductId $id          the product id
+     * @param string    $id          the product id
      * @param string    $reference   the product reference
      * @param string    $name        the product name
      * @param string    $description the product description
      * @param float     $price       the product price
      */
     public function __construct(
-        public ProductId $id,
+        public string $id,
         public string $reference,
         public string $name,
         public string $description,
