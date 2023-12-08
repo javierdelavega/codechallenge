385d44a0a89b81f8384e6a3c3a322dffcec852d2
Javier de la Vega <javier@smartidea.es>
2023-12-08 01:17:03 +0100

    refactor: add final and readonly to command

--- src/Codechallenge/Billing/Application/Command/AddProductCommand.php
+++ src/Codechallenge/Billing/Application/Command/AddProductCommand.php
@@ -8,10 +8,10 @@ use App\Codechallenge\Auth\Domain\Model\UserId;
 use App\Codechallenge\Billing\Application\Service\Cart\AddProductRequest;
 use App\Codechallenge\Shared\Domain\Bus\Command\Command;
 
-class AddProductCommand implements Command
+final readonly class AddProductCommand implements Command
 {
   public function __construct(
-    public readonly UserId $userId,
-    public readonly AddProductRequest $request,
+    public UserId $userId,
+    public AddProductRequest $request,
   ){ }
 }
